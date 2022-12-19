fb59309bb078ab4902143da93f08d9c367d740a1
Janko <jankokondic84@gmail.com>
2022-12-19 21:52:49 +0100

    set up

---
+++ Task1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    printf("asdsad asd asd asd\n");
+}
---
+++ Task2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int NUMBER_OF_ATTEMPTS = 10;
+
+int main(){
+    srand(time(NULL));
+    printf("asdsad asd asd asd \n");
+}
