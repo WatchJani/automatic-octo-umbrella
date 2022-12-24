72db2eacebb804d18b20b7ff2d906741912b6a28
Janko <jankokondic84@gmail.com>
2022-12-24 18:53:23 +0100

    Print game

--- Task2/main.c
+++ Task2/main.c
@@ -62,10 +62,6 @@ char *load_word(){
     return my_word;
 }
 
-bool isFind(char letter){
-    
-}
-
 int myLength(char my_word[]){
     int index = 0;
     while (my_word[index] != '\0' ){
@@ -74,16 +70,29 @@ int myLength(char my_word[]){
     return index;
 }
 
+void PrintGame(char game[], int length){
+    for(int i = 0; i < length; i++){
+        printf("%c", game[i]);
+    }
+    printf("\n");
+}
+
 int main(){
     srand(time(NULL));
     char *my_word = load_word();
     bool game_end = true;
+    
+    int length = myLength(my_word);
 
     printf("%s\n", my_word);
-    printf("%i \n", myLength(my_word));
-    
 
-    while ( NUMBER_OF_ATTEMPTS > 0){
+    char game[length];
+
+    for(int i = 0; i< length;i++){
+        game[i] = '_';
+    }
+
+    while(game_end){
         //printf("%i \n", NUMBER_OF_ATTEMPTS);
         char my_letter;
         scanf("%s", &my_letter);
@@ -91,12 +100,18 @@ int main(){
         int index = 0;
         while (my_word[index] != '\0' ){
             if(my_word[index] == my_letter ){
-                printf("yes \n");
+                game[index] = my_letter;
             }
             index++;
         }
         NUMBER_OF_ATTEMPTS--;
+        PrintGame(game, length);
+        // if(game == my_word){
+        //     game_end = false;
+        // }
     }
+
+    
 }
 
 
